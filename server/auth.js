b22a7e27cf54e3c45145cd8cb90b85fd8553ce63
Miguel Morujão <miguel.morujao@edgeinnovation.hq>
2016-06-22 15:59:19 +0100

    started working on account merging

--- server/auth.js
+++ server/auth.js
@@ -36,3 +36,46 @@ ServiceConfiguration.configurations.insert({
   secret: "7IYk7TcC0FmTfcEkJY9MeyIoIEe7DqSlkBLIy2rCHdkr2nukR5"
 });
 
+
+
+    Accounts.onCreateUser(function(options, user) {
+        if (user.services) {
+            
+            var service = _.keys(user.services)[0];
+            var email = user.services[service].email;
+ 
+            console.log(email);
+
+ 
+            if (!email)
+                return user;
+ 
+            // see if any existing user has this email address, otherwise create new
+            var existingUser = Meteor.users.findOne({'emails.address': email});
+            
+            
+            if (!existingUser)
+                return user;
+
+            console.log(existingUser._id);
+            console.log("he");
+
+ 
+            // precaution, these will exist from accounts-password if used
+            if (!existingUser.services)
+                existingUser.services = { resume: { loginTokens: [] }};
+            if (!existingUser.services.resume)
+                existingUser.services.resume = { loginTokens: [] };
+ 
+            // copy across new service info
+            existingUser.services[service] = user.services[service];
+            existingUser.services.resume.loginTokens.push(
+                user.services.resume.loginTokens[0]
+            );
+ 
+            // even worse hackery
+            Meteor.users.remove({_id: existingUser._id}); // remove existing record
+            return existingUser;                          // record is re-inserted
+        }
+    });
+ 
