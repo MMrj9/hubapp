5c562880f805403dc1303f600a6c583caa52f61d
Miguel Morujão <miguelmorujao@gmail.com>
2016-09-02 16:08:02 +0100

    Addded user roles package and role change feature

--- client/templates/users/user-profile.js
+++ client/templates/users/user-profile.js
@@ -5,6 +5,20 @@ Template.userProfile.onCreated(function() {
   });
 });
 
+
+Template.userProfile.rendered = function() {
+    //Maneira estúpida de dar a volta á situação
+        this.autorun(function() {
+            var optionsCursor = Country.find().count();
+            if (optionsCursor > 0) {
+                $('select').material_select();
+            }
+        });
+      };
+
+
+
+
 Template.userProfile.helpers({
   'user': function(){
     return Meteor.users.find({_id: FlowRouter.getParam("userId")}).fetch()[0];
@@ -30,6 +44,17 @@ Template.userProfile.helpers({
    }
 	  else 
 		  return false;
-  }
+  },
+  'rolesList': function(){
+    return ["User", "Admin", "InternalCollaborator", "ExternalCollaborator"]
+  } 
+
 });
 
+Template.userProfile.events({
+  'change #role-select': function (event, template) {
+        var role = $(event.currentTarget).val();
+        Roles.removeUsersFromRoles( Meteor.userId(), Meteor.user().roles);
+    Roles.addUsersToRoles( Meteor.userId(), [ role ]);
+    }
+})
