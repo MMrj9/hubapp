f9c420c2c7e3fea29c4365a4f1237e95dc7bea16
Miguel Morujão <miguelmorujao@gmail.com>
2016-09-13 17:08:31 +0100

    more Idea Contest (Rise Above) tweaks and fixed the date/time concat problem

--- client/templates/events/event_new.js
+++ client/templates/events/event_new.js
@@ -1,46 +1,30 @@
 Template.eventnew.rendered = function() {
     $('.datepicker').pickadate({
             selectMonths: true, // Creates a dropdown to control month
-            selectYears: 1
+            selectYears: 1,
+            format: 'dd-mm-yyyy'
         });
-
-     $('#timepicker').pickatime({
-        autoclose: false,
-        twelvehour: false
-      });
 
 };
 
 Template.eventnew.events({
     'click #create-event': function(e) {
 
-        combineDateAndTime = function(date, time) {
-            console.log(time);
-    timeString = time.getHours() + ':' + time.getMinutes() + ':00';
+        var startDateTime = moment($('#beginDate').val() + ' ' + $('#beginTime').val(), "DD-MM-YYYY HH:mm").toDate();
+        var endDateTime = moment($('#endDate').val() + ' ' + $('#endTime').val(), "DD-MM-YYYY HH:mm").toDate();
 
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1; // Jan is 0, dec is 11
-    var day = date.getDate();
-    var dateString = '' + year + '-' + month + '-' + day;
-    var combined = new Date(dateString + ' ' + timeString);
-
-    return combined;
-};
-       
         var event = { data:{
             name: $('name').val(),
             //formatação do texto
             description: decodeURIComponent(encodeURIComponent($('#description').val())),
-            start: combineDateAndTime($('#beginDate').val(),$('#beginTime').val()),
-            end: combineDateAndTime($('#endDate').val(),$('#endTime').val()),
+            start: startDateTime,
+            end: endDateTime,
             place: $('#place').val(),
             city: $('#city').val(),
             status: "new"
         }
         };
 
-
-    
         //Check if user is logged in
         if (Meteor.userId() != null) {
                 Meteor.call('eventInsert', event, function(error, eventId) {
